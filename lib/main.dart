cb2ea1773ec913476e5b3a1c0c7b41ed113750ac
pasinyasothorn <121300542+pasinyasothorn@users.noreply.github.com>
2023-01-15 11:51:28 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -93,7 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ]
             ),
           ),
-          Container(width: 300,height: 50,color: Colors.green),
+         
         ],
 
       ),
